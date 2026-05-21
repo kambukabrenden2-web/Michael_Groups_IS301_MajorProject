324ca6bf55f08c3cacd880cf58971410739fc35a
kambukabrenden2-web <kambukabrenden2@gmail.com>
2026-05-21 14:32:44 +1000

    Added main.cand authentication

--- main.c
+++ main.c
@@ -1,1 +1,13 @@
 
+#include <stdio.h>
+#include "auth.h"
+#include "menu.h"
+
+int main() {
+    if (!login()) {
+        printf("Exiting system.\n");
+        return 1;
+    }
+    runMainMenu();
+    return 0;
+}
